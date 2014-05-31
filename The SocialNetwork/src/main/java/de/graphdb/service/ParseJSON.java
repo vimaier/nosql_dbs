038762ce79b3703ee5faff5e6d491d91cb7e048c
anho0014 <anho0014@stud.fh-kl.de>
2014-05-31 11:43:49 +0200

    impl: updateUser, deleteUser, makeFriends, findFriends(es fehlt JSONString parsen) --> Neo4j

---
+++ The SocialNetwork/src/main/java/de/graphdb/service/ParseJSON.java
@@ -0,0 +1,18 @@
+package de.graphdb.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import de.graphdb.dto.UserDTO;
+
+public class ParseJSON {
+	
+	public static Collection<UserDTO> parseString(String jsonString){
+		Collection<UserDTO> users = new ArrayList<UserDTO>();
+		
+		//TODO: parse jsonString
+		
+		return users;
+	}
+
+}
