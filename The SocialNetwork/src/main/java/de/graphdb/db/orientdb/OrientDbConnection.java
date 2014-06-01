e182340c6a3b763ad68f4a998ee3599f1dfe9b85
julian.neuhaus <julian.neuhaus@brain-activit.com>
2014-06-01 17:45:38 +0200

    added orientdb dao and connection class

---
+++ The SocialNetwork/src/main/java/de/graphdb/db/orientdb/OrientDbConnection.java
@@ -0,0 +1,88 @@
+package de.graphdb.db.orientdb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
+
+public class OrientDbConnection
+{
+	private static Logger log = LoggerFactory.getLogger(OrientDbConnection.class);
+	
+	private OrientGraphFactory graphFactory;
+	
+	private final String username = "admin";
+	private final String password = "admin";
+	private final String databaseProtocoll = "remote";
+	private final String databaseHost = "10.0.3.44";
+	private final String databaseName = "test";
+	private final String databaseUrl = databaseProtocoll + ":" + databaseHost + "/" + databaseName;
+	
+	private enum SINGLETON
+	{
+		INSTANCE;
+		private final OrientDbConnection connectionFactory;
+
+		private SINGLETON()
+		{
+			connectionFactory = new OrientDbConnection();
+		}
+
+		private OrientDbConnection getConnection()
+		{
+			return connectionFactory;
+		}
+	}
+	
+	private OrientDbConnection()
+	{
+		
+	}
+
+	public static OrientDbConnection getInstance()
+	{
+		log.debug("get instance of conncetionfactory");
+		return SINGLETON.INSTANCE.getConnection();
+	}
+	
+	public boolean close()
+	{
+		log.debug("try to close database");
+		
+		if(this.graphFactory != null)
+		{
+			graphFactory.close();
+		}
+		else
+		{
+			log.debug("GraphDatabaseConnection is null");
+		}
+
+		log.debug("Closed database");
+		
+		return true;
+	}	
+	
+	protected OrientGraphFactory getGraphFactory()
+	{
+		log.debug("try to get GraphFactory");
+		
+		if(this.graphFactory == null || this.graphFactory.getDatabase().isClosed())
+		{
+			open();
+		}
+		
+		log.debug("return GraphFactory");
+		
+		return this.graphFactory;
+	}
+	
+	protected void open()
+	{	
+		log.debug("try to open database");
+		
+		this.graphFactory = new OrientGraphFactory(this.databaseUrl,this.username,this.password).setupPool(10,10);		
+		
+		log.debug("Succesfully opened database");
+	}
+}
---
+++ The SocialNetwork/src/main/java/de/graphdb/db/orientdb/OrientDbDao.java
@@ -0,0 +1,285 @@
+package de.graphdb.db.orientdb;
+
+import java.util.Collection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.impls.orient.OrientGraph;
+
+import de.graphdb.db.GraphDBInterface;
+import de.graphdb.dto.UserDTO;
+
+public class OrientDbDao implements GraphDBInterface
+{
+	private final static Logger log = LoggerFactory.getLogger(OrientDbDao.class);
+	
+	private final OrientDbConnection connection;
+	
+	public OrientDbDao()
+	{
+		this.connection = OrientDbConnection.getInstance();
+	}
+	
+	private OrientGraph getGraph()
+	{
+		try
+		{
+			return this.connection.getGraphFactory().getTx();
+		}
+		catch(Exception e)
+		{
+			log.error("Couldn't create graphTx",e);
+			return null;
+		}		
+	}
+	
+	private void convertUserToVertex(Vertex v,UserDTO u)
+	{
+		v.setProperty("forename", u.getForename());
+		v.setProperty("surname", u.getSurname());
+		v.setProperty("city", u.getCity());
+		v.setProperty("housenumber", u.getHousenumber());
+		v.setProperty("postcode", u.getPostcode());
+		v.setProperty("street", u.getStreet());
+		v.setProperty("email", u.getMailadress());
+		v.setProperty("password", u.getPassword());		
+	}
+	
+	private void convertVertexToUser(Vertex v,UserDTO u)
+	{
+		u.setForename((String) v.getProperty("forename"));
+		u.setSurname((String) v.getProperty("surname"));
+		u.setCity((String) v.getProperty("city"));
+		u.setHousenumber((String) v.getProperty("housenumber"));
+		u.setPostcode((String) v.getProperty("postcode"));
+		u.setStreet((String) v.getProperty("street"));
+		u.setMailadress((String) v.getProperty("email"));
+		u.setPassword((String) v.getProperty("password"));	
+	}
+	
+	@Override
+	public boolean insertUser(UserDTO user)
+	{
+		if(user == null)
+		{
+			log.debug("UserObject is null -> abort insert");
+			return false;
+		}
+		
+		OrientGraph g = getGraph();
+		
+		if(g != null)
+		{
+			try
+			{
+				if(user.getId() != null && !user.getId().isEmpty() && g.getVertex(user.getId()) != null)
+				{
+					log.error("Tried to insert an alreay existing user");
+					g.commit();
+					return false;
+				}
+				else
+				{
+					Vertex v = g.addVertex(null);
+					
+					convertUserToVertex(v, user);
+					
+					g.commit();
+					
+					user.setId(v.getId().toString());
+					
+					log.debug("stored new user with the id: " + user.getId());
+					return true;
+				}
+			}
+			catch(Exception e)
+			{
+				log.error("Couldn't insert new user",e);
+			}
+			finally
+			{
+				g.shutdown();
+			}			
+		}	
+		
+		return false;
+	}
+
+	@Override
+	public UserDTO updateUser(UserDTO user)
+	{
+		if(user == null)
+		{
+			log.debug("UserObject is null -> abort update");
+			return null;
+		}
+		
+		OrientGraph g = getGraph();
+		
+		if(g != null)
+		{
+			try
+			{
+				Vertex toUpdate = g.getVertex(user.getId());
+				
+				if(user.getId() == null || user.getId().isEmpty() || toUpdate == null)
+				{
+					log.error("Tried to update a not existing user");
+					g.commit();
+					return null;
+				}
+				else
+				{
+					convertUserToVertex(toUpdate, user);
+					
+					g.commit();
+					
+					log.debug("update user with the id: " + user.getId());
+					return user;
+				}
+			}
+			catch(Exception e)
+			{
+				log.error("Couldn't update user");
+			}
+			finally
+			{
+				g.shutdown();
+			}			
+		}	
+		
+		return null;
+	}
+	
+	public UserDTO getUserById(String id)
+	{
+		if(id == null || id.isEmpty())
+		{
+			log.debug("recieved empty id");
+			return null;
+		}
+		
+		OrientGraph g = getGraph();
+		
+		if(g != null)
+		{
+			try
+			{
+				Vertex v = g.getVertex(id);
+				g.commit();
+				if(v == null)
+				{
+					log.debug("Found no user with the id: " + id);
+					return null;
+				}
+				else
+				{
+					UserDTO toReturn = new UserDTO();
+					toReturn.setId(id);
+					convertVertexToUser(v, toReturn);
+					return toReturn;
+				}				
+			}
+			catch(Exception e)
+			{
+				log.error("Couldn't search for user with id: " + id);
+			}
+			finally
+			{
+				g.shutdown();
+			}			
+		}	
+		
+		return null;
+	}
+
+	@Override
+	public boolean deleteUser(UserDTO user)
+	{
+		if(user == null)
+		{
+			log.debug("UserObject is null -> abort delete");
+			return false;
+		}
+		
+		OrientGraph g = getGraph();
+		
+		if(g != null)
+		{
+			try
+			{
+				Vertex toDelete = g.getVertex(user.getId());
+				
+				if(user.getId() == null || user.getId().isEmpty() || toDelete == null)
+				{
+					log.error("Tried to delete an not existing user");
+					g.commit();
+					return false;
+				}
+				else
+				{
+					g.removeVertex(g.getVertex(toDelete));
+					g.commit();
+					
+					user.setId(null);
+					return true;
+				}
+			}
+			catch(Exception e)
+			{
+				log.error("Couldn't delete new user",e);
+			}
+			finally
+			{
+				g.shutdown();
+			}			
+		}	
+		
+		return false;
+	}
+
+	@Override
+	public Collection<UserDTO> findFriends(UserDTO user)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<UserDTO> findUsers(String suchbegriff)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean makeFriends(UserDTO user, UserDTO friend)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean unfriend(UserDTO user, UserDTO friend)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Collection<UserDTO> findFriendsOfFriends(Collection<UserDTO> friends)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<UserDTO> findRelative(UserDTO user)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
