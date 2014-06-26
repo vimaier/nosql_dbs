6135ab1a0bdca732ce7a9d7cb870f81a742629bc
Ronny Bölter <robo0001@stud.fh-kl.de>
2014-06-26 10:32:23 +0200

    Update OrientDB.java

--- LoadTest/src/de/graphdb/de/OrientDB.java
+++ LoadTest/src/de/graphdb/de/OrientDB.java
@@ -1,46 +1,135 @@
 package de.graphdb.de;
 
-public class OrientDB extends GraphDB 
-{
+import com.orientechnologies.orient.core.config.OGlobalConfiguration;
+import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.impls.orient.OrientGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
 
-	/**
-	 * Konstruktor
-	 */
-	public OrientDB()
-	{
-		this.databaseName = "OrientDB";
-	}
-	
-	@Override
-	public void connect() {
-		//verbinde zur Datenbank und speichere client in this.client
-	}
+public class OrientDB extends GraphDB {
 
-	@Override
-	public void disconnect() {
-		//schließe Datenbankverbindung
-	}
-	
-	@Override
-	public void clearDatabase() {
-		//lösche alle Datenbankeinträge		
-	}	
+  private OrientGraphFactory graphFactory;
 
-	@Override
-	public void executeQuery(String query) {
-		//Führe Query aus query in Datenbank aus
-	}
+  private final String username = "admin";
+  private final String password = "admin";
+  private final String databaseProtocoll = "remote";
+  private final String databaseHost = "10.0.3.44";
+  private final String databaseName = "test";
+  private final String databaseUrl = databaseProtocoll + ":" + databaseHost + "/" + databaseName;
 
-	@Override
-	public void executeReadVertex(int vertexName) {
-		//lese Knoten mit property 'name' und Wert vertexName aus aus der 
-		//Datenbank aus
-	}
+  /**
+   * Konstruktor
+   */
+  public OrientDB() {
+    super.databaseName = "OrientDB";
+  }
 
-	@Override
-	public void executeWriteVertex(int vertexName) {
-		//Füge Knoten mit Property 'name' und Wert aus vertexName in die 
-		//Datenbank ein
-	}
+  private enum SINGLETON {
+    INSTANCE;
+    private final OrientDB connectionFactory;
 
+    private SINGLETON() {
+      connectionFactory = new OrientDB();
+    }
+
+    private OrientDB getConnection() {
+      return connectionFactory;
+    }
+  }
+
+  public static OrientDB getInstance() {
+    return new OrientDB();
+  }
+
+  @Override
+  public void connect() {
+    super.client = OrientDB.getInstance();
+  }
+
+  @Override
+  public void disconnect() {
+    if (this.graphFactory != null) {
+      graphFactory.close();
+    }
+  }
+
+  @Override
+  public void clearDatabase() {
+    OrientGraph g = getGraph();
+    if (g != null) {
+      try {
+        g.command(new OSQLSynchQuery<Vertex>("delete from V")).execute();
+        g.commit();
+      } catch (Exception e) {
+        e.printStackTrace();
+        g.rollback();
+      } finally {
+        g.shutdown();
+      }
+    }
+  }
+
+  @Override
+  public void executeQuery(String query) {
+    OrientGraph g = getGraph();
+    if (g != null) {
+      try {
+        g.command(new OSQLSynchQuery<Vertex>(query)).execute();
+        g.commit();
+      } catch (Exception e) {
+        e.printStackTrace();
+        g.rollback();
+      } finally {
+        g.shutdown();
+      }
+    }
+  }
+
+  @Override
+  public void executeReadVertex(int vertexName) {
+    OrientGraph g = getGraph();
+    if (g != null) {
+      try {
+        g.getVertex(vertexName);
+        g.commit();
+      } catch (Exception e) {
+        e.printStackTrace();
+        g.rollback();
+      } finally {
+        g.shutdown();
+      }
+    }
+  }
+
+  @Override
+  public void executeWriteVertex(int vertexName) {
+    OrientGraph g = getGraph();
+    try {
+      Vertex v = g.addVertex("V");
+      v.setProperty("name", vertexName);
+      g.commit();
+    } catch (Exception e) {
+      e.printStackTrace();
+      g.rollback();
+    } finally {
+      g.shutdown();
+    }
+  }
+
+  private OrientGraph getGraph() {
+    try {
+      return ((OrientDB) super.client).getGraphFactory().getTx();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  protected OrientGraphFactory getGraphFactory() {
+    if (this.graphFactory == null || this.graphFactory.getDatabase().isClosed()) {
+      OGlobalConfiguration.CLIENT_CHANNEL_MAX_POOL.setValue(50000);
+      this.graphFactory = new OrientGraphFactory(this.databaseUrl, this.username, this.password);
+    }
+    return this.graphFactory;
+  }
 }
